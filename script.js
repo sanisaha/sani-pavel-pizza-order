ba5dee928e6e6f0c40bc446923ee40afc1518a4f
Pavel-Kliukin <9213087@mail.ru>
2023-02-06 23:37:54 +0200

    Added total amount calculation and bug of toppings output fixed

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
-
-let totalPrice = document.getElementById('totalPrice');
 let pizza_price = 7.5;
-const topping_price = 0;
-const service_price = 0;
+let topping_price = 0;
+let service_price = 0;
 const pizzaPrice = (id, value, title) => {
     const pizzaPrice = document.querySelector('#pizza-price');
     const pizzaDetails = document.querySelector('#pizza-details');
@@ -13,46 +11,56 @@ const pizzaPrice = (id, value, title) => {
     }
 } 
 let toppingsArr = [];
-function selectToppings (id, value) {
+function selectToppings (value) {
     const toppings = document.querySelector('#toppings');
     const toppingPrice = document.querySelector('#topping-price');
-    
-    if (id = 'goat'){
-        if (toppingsArr.includes(value)){
-            toppingsArr.pop(value);
-        }
 
-        else {
-            toppingsArr.push(value);
-        }
-        
+    if (toppingsArr.includes(value)){
+        toppingsArr.splice(toppingsArr.indexOf(value), 1); //This is the right way to delete exact item from an array insted of .pop()
+        console.log(toppingsArr);
+    } else {
+        toppingsArr.push(value);
+        console.log(toppingsArr);
     }
+        
     let str = '';
     for (const topping of toppingsArr){
-        str += topping + ' ';
+        str += topping + ', ';
     }
     toppings.textContent = str;
-    console.log(toppingsArr.length);
+
     if (toppingsArr.length <= 4){
-        toppingPrice.textContent = 0;
+        toppingPrice.textContent = '0 €';
+        topping_price = 0;
     }
     else {
-        toppingPrice.textContent = 0.5 * (toppingsArr.length - 4);
+        topping_price = 0.5 * (toppingsArr.length - 4);
+        toppingPrice.textContent = topping_price.toFixed(2) + '€';
     }
-
 }
-    const deliveryPrice = document.querySelector('#service-price');
-    const service = document.querySelector('#service');
+const deliveryPrice = document.querySelector('#service-price');
+const service = document.querySelector('#service');
 
-    const serviceDetails = document.querySelector('#selectService');
-    const homeService = document.getElementById('home');
-    const eatService = document.getElementById('eat');
-    const pickService = document.getElementById('pick');
-    serviceDetails.addEventListener('change', function getPrice(){
-        deliveryPrice.innerText = this.value;
-    })
-    // let totalPrice = document.getElementById('totalPrice')
-    // const pizza_price = document.getElementById('pizza-price');
-    // const topping_price = document.getElementById('topping-price').innerText;
-    // const service_price = document.getElementById('service-price').innerText;
-    console.log(pizza_price, topping_price, service_price);
+const serviceDetails = document.querySelector('#selectService');
+const homeService = document.getElementById('home');
+const eatService = document.getElementById('eat');
+const pickService = document.getElementById('pick');
+
+serviceDetails.addEventListener('change', function getPrice(){
+    deliveryPrice.innerText = this.value;
+    service_price = +this.value;
+})
+
+let totalPriceChange = document.querySelectorAll('.part');
+console.log(totalPriceChange);
+
+//document.querySelectorAll doesn't have addEventListener method. So we have to make for loop to listen all possible events
+for(let i=0; i<totalPriceChange.length; i++){
+    totalPriceChange[i].addEventListener('change', priceChange);
+}
+
+let totalPrice = document.getElementById('totalPrice');
+
+function priceChange() {
+    totalPrice.textContent = (pizza_price + topping_price + service_price).toFixed(2) + ' €';
+}
